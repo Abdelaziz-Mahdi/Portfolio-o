5b766ba04cdd91fe267a63f15e3d218ff2ac0990
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2022-12-20 17:15:16 +0200

    Add-mobile-menu-js

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function myFunction() {
+  document.getElementById("ani").style.marginLeft = "100%";
+    window.location.href = "menu.html";
+  }
+
+  function headding() {
+    window.location.href = "index.html";
+  }
+
+  function worksSection() {
+    window.location.href = "index.html#works";
+  }
+
+  function aboutSection() {
+    window.location.href = "index.html#aboutMyself";
+  }
+
+  function footerSection() {
+    window.location.href = "index.html#footerV";
+  }
