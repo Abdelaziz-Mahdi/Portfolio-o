278730aa7a8fa3f2282be96a642342ffb02158c7
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2022-12-20 20:35:52 +0200

    Fix-eslint-errors

--- scripts.js
+++ scripts.js
@@ -1,30 +1,30 @@
 // eslint-disable-next-line  no-unused-vars
 function openMenu() {
-  document.getElementById("ani").style.marginLeft = "100%";
-  window.location.href = "menu.html";
+  document.getElementById('ani').style.marginLeft = '100%';
+  window.location.href = 'menu.html';
 }
 
 // eslint-disable-next-line  no-unused-vars
 function headding() {
-  window.location.href = "index.html";
+  window.location.href = 'index.html';
 }
 
 // eslint-disable-next-line  no-unused-vars
 function worksSection() {
-  window.location.href = "index.html#works";
+  window.location.href = 'index.html#works';
 }
 
 // eslint-disable-next-line  no-unused-vars
 function aboutSection() {
-  window.location.href = "index.html#aboutMyself";
+  window.location.href = 'index.html#aboutMyself';
 }
 
 // eslint-disable-next-line  no-unused-vars
 function footerSection() {
-  window.location.href = "index.html#footerV";
+  window.location.href = 'index.html#footerV';
 }
 
-const mediaQuery = window.matchMedia("(min-width: 768px)");
+const mediaQuery = window.matchMedia('(min-width: 768px)');
 
 function screenSize(i) {
   if (i.matches) {
